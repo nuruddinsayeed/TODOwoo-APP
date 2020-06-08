111e6912dafd99d8c8d45ed5bca0ac08e938bacb
Sayed <nuruddinsayeed@gmail.com>
2020-06-08 21:35:13 +0600

    User erg system added

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html')
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form' : UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+            user.save()
+        else:
+            #Tell user password didn't match
+            print("Hello world")
